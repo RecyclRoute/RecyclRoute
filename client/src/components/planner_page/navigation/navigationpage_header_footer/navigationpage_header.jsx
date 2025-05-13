a9278918bc774f51de5cfb9fc18508e25271aa01
micka-t <micha.tschanz@students.fhnw.ch>
2025-05-13 18:26:53 +0200

    20250513_MTSC_7

--- client/src/components/planner_page/navigation/navigationpage_header_footer/navigationpage_header.jsx
+++ client/src/components/planner_page/navigation/navigationpage_header_footer/navigationpage_header.jsx
@@ -1,16 +1,46 @@
 import "./navigationpage_header.css";
+import { Link } from "react-router-dom";
+import recyclroute_logo from "../../../icons/RecyclRoute_Logo_9E9E9E.png";
+
 
 export const NavigationpageHeader = (props) => {
 
 
   return (
     <header className="PlannerpageHeader" position="static">
-      <div>
-        <div id="HeaderTitle">
-          RecyclRoute - Navigation
-        </div>
-        <div id="HeaderUsername"></div>
-      </div>
+  <div style={{
+          flex: 1,
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          position: "relative",
+          overflow: "auto"
+  }}>
+    <div id="HeaderTitle" style={{
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "center"}}>
+      <Link
+        to="/"
+        style={{
+          textDecoration: 'none',
+          color: 'inherit',
+          flex: 1,
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          position: "relative",
+          overflow: "auto",
+          marginRight: "5px"
+        }}
+      >
+        <img src={recyclroute_logo} style={{maxHeight: "50px", marginRight: "5px"}}></img>
+        RecyclRoute
+      </Link>
+      <div style={{marginRight: "5px"}}>{" - "}</div>
+      <div>{" Navigation"}</div>
+    </div>
+  </div>
     </header>
   );
 };
